f78d1214f62f0b68f75bb2bd7c1bf0e3a30d439e
Edgard Jonathan Carrion Silva <edgard.jonthc@gmail.com>
2022-07-23 17:29:40 -0500

    pagina de fotografia desarrollada con react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  Testimonio  from "./componentes/Testimonio";
+//exportaciones nombradas {Testimonio} o esportaciones por defecto   export default Testimonio;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      <Testimonio 
+        nombre = 'Edgard'
+        img = 'img1'
+      />
+      <Testimonio 
+        nombre = 'Edgard'
+        img = 'img2'
+      />
+      <Testimonio 
+        nombre = 'Edgard'
+        img = 'img3'
+      />
+      </div>
+     
     </div>
   );
 }
